9c384b6d78d0cb82ac5de81822dc9fd2cba15a39
kilbergr <rekilberg@gmail.com>
2015-06-28 21:45:32 -0700

    progress on request APIs

--- app.js
+++ app.js
@@ -26,9 +26,19 @@ app.use(morgan('tiny'));
  			distStates.push(arr[i]);
  		};
  	}
- //	console.log(distStates);
-
+ 	return distStates;
  };
+
+//getting states' APIs
+var mapStates=function(arr){
+	var apiArr = [];
+	for(var j = 0; j < arr.length; j++){
+		var state = encodeURIComponent(arr[j]);
+		var apiUrl = 'http://maps.googleapis.com/maps/api/geocode/json?address=' + state;
+		apiArr.push(apiUrl);
+	}
+	return apiArr;
+}
 
 
 
@@ -98,11 +108,35 @@ app.post('/recalls', function(req, res){
 	  var recalls = JSON.parse(body).results;
 	  recalls.forEach(function(recall){
 	  	var add = new RegExp(recall.distribution_pattern);
-	  	console.log(add);
+	  	//console.log(add);
+	  	//find the states that appear in the distribution list
+	  	var distStates = findState(add, stateNames);
+	  	//find the google maps api URL for each state
+			var apiUrl = mapStates(distStates);
+			//make a request using that api URL
+			 request(apiUrl, function(error, response, body) {
+        if (error) {
+          res.status(500).send('Server Error');
+        } 
+        else if (!error && response.statusCode === 200) {
+          var data = JSON.parse(body);
+          var state = {};
+          if (data.results && data.results.length >= 1 &&
+              data.results[0].geometry && data.results[0].geometry.location) {
+            state.lat = data.results[0].geometry.location.lat;
+            state.long = data.results[0].geometry.location.lng;
+            //WHAT TO DO NOW???
+            console.log(state);
+
+          } else {
+            res.status(422).send('Place not found');
+          }
+        } else {
+          res.status(500).send('Server Error');
+        }
+      });
+    }
 	  })
-
-
-
 		res.render('recalls/index', {recalls:recalls});
 	}
 	})
